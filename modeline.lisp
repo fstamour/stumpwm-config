86851551be04966f8177f823199c95dfa7f6ac4d
Francis St-Amour <m_psyco2@hotmail.com>
2020-03-01 21:36:07 -0500

    tweak modeline colors

--- modeline.lisp
+++ modeline.lisp
@@ -1,24 +1,34 @@
 
 (in-package #:stumpwmrc)
 
-(defun toggle-mode-line-eveywhere ()
+(defun enable-mode-line-eveywhere ()
   (loop for screen in *screen-list* do
     (loop for head in (screen-heads (current-screen)) do
-	    (toggle-mode-line screen head))))
+	    (enable-mode-line screen head t))))
 
+(enable-mode-line-eveywhere)
+
+;; *colors*
+;; => ("black" "red" "green" "yellow" "blue" "magenta" "cyan" "white")
+;;     0       1     2       3        4      5         6      7
 
+
 ;; Default *screen-mode-line-format*
 ;; => "[^B%n^b] %W"
 
 (setf *screen-mode-line-format*
-      "[^B%d^b] [^B%g^b] [%W ]")
-;;      Time    Groups   Windows
+      `("[^B%g^b] [%W ] ^>"
+;;           Groups Windows
+       ;;  '(:eval (stumpwm:run-shell-command "date" t))
+        ;; (:eval (bar 50 10 #\x #\-))
+        "^5[^B%d^b]"))
 
+
 ;; Default *window-format*
 ;; "%m%n%s%50t"
 
 (setf *window-format*
-      "%m%n%s%10c")
+      "^2%m%n^n^1%s^n%10c")
 #|
 %m Draw a # if the window is marked.
 %n The window's number
@@ -29,8 +39,7 @@
 %10c The windows' class, cropped to 10 characters
 |#
 
-(toggle-mode-line-eveywhere)
-
+
 ;; Default is 60.
 (setf *mode-line-timeout* 1)
 (stumpwm::turn-on-mode-line-timer)

3b24f5d0522b0564d876f8f35c3203879efd00c1
Francis St-Amour <m_psyco2@hotmail.com>
2020-04-18 23:36:33 -0400

    tweak the modeline and the colors

--- modeline.lisp
+++ modeline.lisp
@@ -1,34 +1,51 @@
 
 (in-package #:stumpwmrc)
 
+;; Used to test changes to modeline's colors
+(defun restart-mode-line-eveywhere ()
+  (loop for screen in *screen-list* do
+    (loop for head in (screen-heads (current-screen)) do
+	 (enable-mode-line screen head nil)
+	 (enable-mode-line screen head t))))
+
 (defun enable-mode-line-eveywhere ()
   (loop for screen in *screen-list* do
     (loop for head in (screen-heads (current-screen)) do
 	    (enable-mode-line screen head t))))
 
+;; The colors must be set before enabling the mode line
+(setf *mode-line-foreground-color* "palegreen" ; Default is Gray50
+      *mode-line-background-color* "Black")    ; Default is Gray20
 (enable-mode-line-eveywhere)
+;; (restart-mode-line-eveywhere)
+
 
 ;; *colors*
 ;; => ("black" "red" "green" "yellow" "blue" "magenta" "cyan" "white")
-;;     0       1     2       3        4      5         6      7
+;;     ^0      ^1    ^2      ^3       ^4     ^5        ^6     ^7
 
 
 ;; Default *screen-mode-line-format*
 ;; => "[^B%n^b] %W"
 
 (setf *screen-mode-line-format*
-      `("[^B%g^b] [%W ] ^>"
-;;           Groups Windows
-       ;;  '(:eval (stumpwm:run-shell-command "date" t))
-        ;; (:eval (bar 50 10 #\x #\-))
+      `("[^B%g^b] [%v ] ^>"
+	;; Groups Windows
+	;; '(:eval (stumpwm:run-shell-command "date" t))
+	;; '(:eval (bar 50 10 #\x #\-))
         "^5[^B%d^b]"))
+
+
+;; Default *mode-line-highlight-template*
+;; => "^R~A^r"
+;; (setf *mode-line-highlight-template* "^B~a^b")
 
 
 ;; Default *window-format*
 ;; "%m%n%s%50t"
 
 (setf *window-format*
-      "^2%m%n^n^1%s^n%10c")
+      "^[^2%m%n^]^[^1%s^]%10c")
 #|
 %m Draw a # if the window is marked.
 %n The window's number

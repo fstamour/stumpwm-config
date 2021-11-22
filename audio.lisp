64cde94d80bb64b5e5945369baa1b0eebdf8375d
Francis St-Amour <m_psyco2@hotmail.com>
2021-11-21 22:02:33 -0500

    fix volume commands on freebsd

--- audio.lisp
+++ audio.lisp
@@ -2,13 +2,49 @@
 (in-package #:stumpwmrc)
 
 (defcommand volume-down () ()
-  (run-shell-command "amixer set Master 500-"))
+  (run-shell-command
+   #-freebsd
+   "amixer set Master 500-"
+   #+freebsd
+   "mixer vol -5"))
 
 (defcommand volume-up () ()
-  (run-shell-command "amixer set Master 500+"))
+  (run-shell-command
+   #-freebsd
+   "amixer set Master 500+"
+   #+freebsd
+   "mixer vol +5"))
 
+#+freebsd
+(defun current-volume ()
+  "On FreeBSD, get the current master volume."
+  (ppcre:register-groups-bind
+      ((#'parse-integer lvol rvol))
+      ("(\\d+):(\\d+)"
+       (run-shell-command "mixer vol" t)
+       :sharedp t)
+    (values lvol rvol)))
+
+(defparameter *volume-before-muting* nil
+  "Variable used to restore to volume to its previous level.")
+
+;; No easy mute on freeBSD for now
 (defcommand mute-toggle () ()
-  (run-shell-command "amixer set Master toggle"))
+  #-freebsd
+  (run-shell-command "amixer set Master toggle")
+  #+freebsd
+  (let ((current-volume (current-volume)))
+    (if (zerop current-volume)
+	;; Currently muted, unmute
+	;; TODO what if *volume-before-muting* is nil?
+	(run-shell-command (format nil "mixer ~d"
+				   *volume-before-muting*))
+	;; Not muted, muting
+	(progn
+	  (setf *volume-before-muting* current-volume)
+	  (run-shell-command "mixer 0")))))
+
+;; for testing: (mute-toggle)
 
 (define-key *top-map* (kbd "XF86AudioLowerVolume") "volume-down")
 (define-key *top-map* (kbd "XF86AudioRaiseVolume") "volume-up")
@@ -18,5 +54,3 @@
 (define-key *top-map* (kbd "XF86AudioStop") "music-stop")
 (define-key *top-map* (kbd "XF86AudioNext") "music-next")
 (define-key *top-map* (kbd "XF86AudioPrev") "music-prev")
-
-

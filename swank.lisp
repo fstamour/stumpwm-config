743dd5f0b60855702dc1a3fd560eca0e1727432d
Francis St-Amour <fr.stamour@gmail.com>
2022-11-28 14:43:20 -0500

    bump slime, improve swank initialization

--- swank.lisp
+++ swank.lisp
@@ -4,44 +4,71 @@
 
 (in-package #:stumpwmrc)
 
+(defparameter +slime-submodule+ "~/.config/stumpwm/slime/")
+
 (defvar *swank-p* nil
   "Flag if swank is started or not")
+
+(defun swank-available-p ()
+  "Test if the swank package is available"
+  ;; already loaded
+  (find-package '#:swank))
+
+(defun swank-started-p ()
+  ""
+  (and
+   ;; loaded but not started
+   (swank-available-p)
+   (or
+    ;; already started
+    *swank-p*
+    ;; already started, but not with the "swank" command
+    (ignore-errors
+     (uiop:symbol-call :swank :connection-info)))))
+
+
+
+(defun load-swank-from (slime-root)
+  (unless (swank-started-p)
+    ;; TODO Check if slime-root exists
+
+    ;; Try to load swank using swank-loader
+    (ignore-errors
+     (with-restarts-menu
+         (load (merge-pathnames "swank-loader.lisp" slime-root))
+       (uiop:symbol-call :swank-loader :init)
+       (return-from load-swank-from)))
+
+    ;; Load the asd file if it swank-loader didn't work for some reason
+    (ignore-errors
+     (with-restarts-menu
+         (unless (asdf:load-asd (merge-pathnames "swank.asd" slime-root))
+           (asdf:load-system '#:swank))))))
+
+(unless (probe-file +slime-submodule+)
+  (message "~&The folder \"~a\" doesn't exists, have you clone the submodule?"
+           +slime-submodule+))
+
+(defcommand load-swank () ()
+  (load-swank-from +slime-submodule+))
+
+
+(defun swank-create-server (port)
+  (uiop:symbol-call
+   :swank :create-server
+   :port port
+   ;; :style swank:*communication-style*
+   :dont-close t)
+  (setf *swank-p* t)
+  (echo-string
+   (current-screen)
+   "Swank started M-x slime-connect RET RET"))
 
 (defcommand swank
     (port) ((:number "Port: "))
   "Starts a swank server on port 4005 and notifies the user."
-  (if (not (find-package 'swank))
-      (message "Swank not loaded...")
-      (if *swank-p*
-	  (message "Swank server already running.")
-	  (progn
-	    (uiop:symbol-call
-	     :swank :create-server
-	     :port port
-	     ;; :style swank:*communication-style*
-	     :dont-close t)
-	    (setf *swank-p* t)
-	    (echo-string
-	     (current-screen)
-	     "Swank started M-x slime-connect RET RET")))))
-
-
-(unless (or
-	 ;; already started
-	 *swank-p*
-	 (and
-	  ;; already loaded
-	  (find-package "swank")
-	  ;; already started, but not with the "swank" command
-	  (uiop:symbol-call :swank :connection-info)))
-
-  ;; Try to load swank using swank-loader (make senses, right?)
-  (ignore-errors
-   (load "~/.config/stumpwm/slime/swank-loader.lisp")
-   (uiop:symbol-call :swank-loader :init))
-
-  ;; Second try...
-  (ignore-errors
-   (unless
-       (load "~/.config/stumpwm/slime/swank.asd")
-     (require 'swank))))
+  (if (not (swank-available-p))
+      (message "Swank not loaded, try the ~a command." #'load-swank)
+      (if (swank-started-p)
+          (message "Swank server already running.")
+          (swank-create-server port))))

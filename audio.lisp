3cd3e983bbdf56231e34b364c7606f86eba4bdb7
Francis St-Amour <fr.stamour@gmail.com>
2024-11-11 09:13:26 -0500

    audio: be able to switch between using pactl and amixer

--- audio.lisp
+++ audio.lisp
@@ -14,27 +14,40 @@
 ;; Check if "amixer" is available
 ;; (sh "bash -c 'command -v ~a'" "amixer")
 
-#++
-(progn
-  (defparameter *last-sh* nil
-    "For debugging purpposes only: keep information about the last time the function sh was called.
+(defparameter *audio-control-style* :amixer)
+
+(defun amixerp ()
+  (eq *audio-control-style* :amixer))
+
+(defun pulse-audio-p ()
+  (eq *audio-control-style* :pulseaudio))
+
+(defparameter *last-sh* nil
+  "For debugging purpposes only: keep information about the last time the function sh was called.
 Trace could be useful too (especially that this only keeps the last invocation.0")
 
-  (defun sh (control-string &rest format-arguments)
-    "Run a command in a shell, wait for it, return its output as a list of lines (strings)."
-    (setf *last-sh* (list control-string format-arguments)) ; for debugging
-    (let* ((command (apply #' format nil control-string format-arguments))
-           (result (uiop:split-string
-                    #++ (run-shell-command command t)
-                    (uiop:run-program
-                     command
-                     :output '(:string :stripped t))
-                    :separator '(#\Newline))))
-      (prog1 result
-        ;; More bebugging stuff
-        (setf (cdr (last *last-sh*))
-              (list command result)))))
+(defun sh (control-string &rest format-arguments)
+  "Run a command in a shell, wait for it, return its output as a list of lines (strings)."
+  (setf *last-sh* (list control-string format-arguments)) ; for debugging
+  (let* ((command (apply #' format nil control-string format-arguments))
+         (result (uiop:split-string
+                  #++ (run-shell-command command t)
+                  (uiop:run-program
+                   command
+                   :output '(:string :stripped t))
+                  :separator '(#\Newline))))
+    (prog1 result
+      ;; More bebugging stuff
+      (setf (cdr (last *last-sh*))
+            (list command result)))))
 
+#++
+(progn
+  (sh "amixer get Master" t)
+  (sh "amixer --device pulse get Master" t))
+
+#++
+(progn
   (let ((sink (car (sh "pactl get-default-sink"))))
     (sh "pactl get-sink-volume ~a" sink))
 
@@ -45,44 +58,73 @@ Trace could be useful too (especially that this only keeps the last invocation.0
     (parse-integer line :start (1+ (position #\/ line)) :junk-allowed t)))
 
 (defun get-output ()
-  (sh "pactl get-default-sink"))
+  (if (pulse-audio-p)
+      (sh "pactl get-default-sink")
+      (error "Tried to call \"get-output\" when pulse-audio is disabled.")))
 
 (defun get-volume (&optional sink)
-  (let* ((sink (or sink
-                   (car (sh "pactl get-default-sink"))))
-         (command-output (sh "pactl get-sink-volume ~a" sink))
-         (volume-string (first command-output)))
-    (when volume-string
-      (parse-integer volume-string
-                     :start (1+ (position #\/ volume-string))
-                     :junk-allowed t))))
-
-#++ (run-shell-command "amixer --device pulse get Master" t)
+  (ccase *audio-control-style*
+    (:amixer
+     (let* ((control "Master")
+            ;; command-output is a list of lines, the grep keeps only the
+            ;; percentages. For example, if you have 2 "outputs" Left and
+            ;; Right, both at 40% the list will be '(40 40).
+            (command-output
+              (sh
+               "amixer get ~a | grep -Po \"[0-9]+(?=%)\""
+               control))
+            (volume-string (first command-output)))
+       (when volume-string
+         (parse-integer volume-string :junk-allowed t))))
+    (:pulseaudio
+     (let* ((sink (or sink
+                      (car (sh "pactl get-default-sink"))))
+            (command-output (sh "pactl get-sink-volume ~a" sink))
+            (volume-string (first command-output)))
+       (when volume-string
+         (parse-integer volume-string
+                        :start (1+ (position #\/ volume-string))
+                        :junk-allowed t))))))
 
 #++
-(let ((sink (car )))
-  (sh "pactl set-sink-volume ~a +5%" sink))
+(get-volume)
 
 (defun set-volume (volume)
   ;; TODO validate volume
   ;; TODO pass device and control
   ;; TODO support jack?
-  (let* ((sink (car (sh "pactl get-default-sink"))))
-    (sh "pactl set-sink-volume ~a ~a" sink volume)
-    (get-volume sink)))
+  (ccase *audio-control-style*
+    (:amixer
+     (let* ((control "Master"))
+       (sh "amixer sset ~a ~d" control volume)
+       (get-volume)))
+    (:pulseaudio
+     (let* ((sink (car (sh "pactl get-default-sink"))))
+       (sh "pactl set-sink-volume ~a ~a" sink volume)
+       (get-volume sink)))))
 
 (defun show-volume (&optional volume)
   (let ((volume (or volume (get-volume))))
     (if volume
+        #++ ;; TODO I don't have volnoti on GuixSD :/
         (sh "volnoti-show ~d" volume)
+        (message "Volume: ~d" volume)
         (message "Failed to get the current volume"))))
 
 (defcommand volume-up (&optional (step 2)) ()
-  (set-volume (format nil "+~d%" step))
+  (set-volume (format nil
+                      (ccase *audio-control-style*
+                        (:amixer "~d%+")
+                        (:pulseaudio "+~d%"))
+                      step))
   (show-volume))
 
 (defcommand volume-down (&optional (step 2)) ()
-  (set-volume (format nil "-~d%" step))
+  (set-volume (format nil
+                      (ccase *audio-control-style*
+                        (:amixer "~d%-")
+                        (:pulseaudio "-~d%"))
+                      step))
   (show-volume))
 
 #++
@@ -93,7 +135,7 @@ Trace could be useful too (especially that this only keeps the last invocation.0
   (volume-up)
   (volume-down))
 
-#++ TODO
+#++ ;; TODO
 (defcommand mute-toggle () ()
   (run-shell-command "amixer set Master toggle"))
 
